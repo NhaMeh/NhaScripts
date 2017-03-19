56703d0c282db3652c382f19acaafadd26962c52
Nha Meh <nhameh@gmail.com>
2017-03-19 05:41:21 +0100

    Updated youtube-dl script.

--- ArchLinux/yt.sh
+++ ArchLinux/yt.sh
@@ -2,8 +2,16 @@
 
 # use a text file with YT link on each line as input
 
-while read line
+DL_DIR="~/Downloads/YouTube/"
+
+if [ ! -d $DL_DIR ]
+then 
+	# make the dir
+	mkdir -p $DL_DIR
+fi
+
+while read LINE
 do
-        echo "$line"
-        youtube-dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' -o '/media/LData/YouTube/%(title)s [%(id)s].%(ext)s' --restrict-filenames $line
+	echo "$LINE"
+	youtube-dl -f "bestvideo[height<=1080]+bestaudio/best[height<=1080]" -o "${DL_DIR}%(title)s [%(id)s].%(ext)s" --restrict-filenames $LINE
 done <$1
